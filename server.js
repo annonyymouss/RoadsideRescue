7be22169fef5bfce5092c5a66ae56141116dcd6e
Akshay <Akshay Sharma>
2024-05-08 13:08:25 +0530

    change in server

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api/user", require("./routes/userroutes"));
-app.use("/api/service", require("./routes/servicerequestroutes"));
-app.use("/api/services", require("./routes/serviceroutes"));
-app.use("/api/review", require("./routes/reviewroutes"));
+app.use("/backend/api/user", require("./routes/userroutes"));
+app.use("/backend/api/service", require("./routes/servicerequestroutes"));
+app.use("/backend/api/services", require("./routes/serviceroutes"));
+app.use("/backend/api/review", require("./routes/reviewroutes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
